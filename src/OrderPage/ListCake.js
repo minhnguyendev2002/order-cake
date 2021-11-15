7bfb2e00965ef577c0173b56db7c37e395f4b243
minhnguyendev2002 <85899672+minhnguyendev2002@users.noreply.github.com>
2021-11-15 10:40:43 +0700

    comit

--- src/OrderPage/ListCake.js
+++ src/OrderPage/ListCake.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import TinderCard from "./CardSwitch"; 
-import ButtonActions from './ButtonActions' 
+import { TinderCard } from "./CardSwitch"; 
+import ButtonActions from './ButtonActions';
 import $ from 'jquery';
 const db = [
   { 
